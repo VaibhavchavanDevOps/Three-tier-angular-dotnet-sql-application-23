17737a050e3a724757f0b4ccb5cfb8eeae2fa624
Vaibhav Chavan <120722478+VaibhavchavanDevOps@users.noreply.github.com>
2024-12-30 13:30:48 +0530

    Update ip.sh

--- ElectronicEquipmentAngular/ip.sh
+++ ElectronicEquipmentAngular/ip.sh
@@ -1,3 +1,3 @@
 sleep 20
 EXTERNAL_IP=$(kubectl get svc --output=jsonpath='{.items[?(@.status.loadBalancer.ingress)].status.loadBalancer.ingress[0].ip}')
-echo "baseServerUrl=http://$EXTERNAL_IP/" > ./ElectronicEquipmentAngular/src/environments/.environment.prod.ts
+echo "baseServerUrl=http://$EXTERNAL_IP:81/" > ./ElectronicEquipmentAngular/src/environments/.environment.prod.ts
